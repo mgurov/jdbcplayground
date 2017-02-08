b2d60e6d33eec4a1c8647d9fc3443206c5a15589
Mykola Gurov <ngurov@gmail.com>
2017-02-08 21:36:27 +0100

    no need for hibernate our excercise

--- src/test/java/com/github/mgurov/jdbcplayground/LogDaoHibernateIT.java
+++ src/test/java/com/github/mgurov/jdbcplayground/LogDaoHibernateIT.java
@@ -1,8 +1,11 @@
 package com.github.mgurov.jdbcplayground;
 
+import org.junit.Ignore;
+
 /**
  * Test talking directly to a postgress database
  */
+@Ignore
 public class LogDaoHibernateIT extends LogDaoIT {
 
     @Override
